e4c9a557384f32ae7cb90a446f3344b1bf824859
DiepNQ <diep4451021@gmail.com>
2025-06-23 00:25:00 +0700

    feat: #3 Integrate Liquibase into Project from Initial Phase

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.liquibase:liquibase-core")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
